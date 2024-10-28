cd4dca3d0451c7a4d3ec48802131558c7d753489
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2024-10-28 20:47:11 +0100

    Ejercicio 12 version 2 actualizado

--- Arbol Dom/Ejercicios/Ejercicio12v2/code.js
+++ Arbol Dom/Ejercicios/Ejercicio12v2/code.js
@@ -1,26 +1,38 @@
 window.onload = () => {
+    let salto = 50;
     resetear();
-    mover();
+    mover(salto);
 }
 
+let timer;
 let i = 1;
 function resetear(){
     let divisito = document.getElementsByTagName("div")[0];
     let boton = document.getElementsByTagName("input")[0];
-
+    
+    
     boton.onclick = function(){
         divisito.style.left = "0px";    
         i = 1;
     }
 }
 
-function mover(){
+function mover(salto){
     let divisito = document.getElementsByTagName("div")[0];
-    let pantalla_final = screen.width;
-    divisito.onmouseover = function (){
-        while((5*i) <= pantalla_final - divisito.clientWidth){
-            divisito.style.left = (5*i) + "px";
-            i++;
-        }
+    divisito.onmouseenter = function () {
+        timer = setInterval(() => {
+            if ((salto* i) <= (screen.width - divisito.offsetWidth)) {
+                this.style.left = (salto * i) + "px";
+                i++;
+            }
+            else{
+                this.style.left =  (screen.width - divisito.offsetWidth) + "px";
+            }
+        }, 50);
     }
+
+    divisito.onmouseleave = function(){
+        clearInterval(timer);
+    }
+    
 }
