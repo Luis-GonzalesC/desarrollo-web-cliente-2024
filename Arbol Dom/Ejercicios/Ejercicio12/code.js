a3205596c4e115bc5a6e59c381fba914a1a22322
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2024-10-28 16:43:22 +0100

    Ejercicio actualizado

--- Arbol Dom/Ejercicios/Ejercicio12/code.js
+++ Arbol Dom/Ejercicios/Ejercicio12/code.js
@@ -7,20 +7,27 @@ window.onload = () => {
 let i = 1;
 function moverDerecha(){
     let divisito = document.getElementsByTagName("div")[0];
-    let boton = document.getElementsByTagName("input")[0];
-
+    let boton = document.getElementsByTagName("input")[1];
+    
+    
     boton.onclick = function(){
-        divisito.style.left = (5*i) + "px";
-        i++;
+        let pantalla_final = screen.width;
+        if((5*i) <= pantalla_final - divisito.clientWidth){      
+            divisito.style.left = (5*i) + "px";
+            i++;
+        }
     }
 }
 
 function moverIzquieda(){
     let divisito = document.getElementsByTagName("div")[0];
-    let boton = document.getElementsByTagName("input")[1];
+    let boton = document.getElementsByTagName("input")[0];
 
     boton.onclick = function(){
-        divisito.style.left = (parseInt(divisito.style.left) - 5) + "px";
-        i--;
+        if (i > 1){
+            divisito.style.left = (parseInt(divisito.style.left) - 5) + "px";
+            i--;
+        }
+        
     }
 }
