d89517fee89b7eaf6d23e53a1f03dcbf2c6f0241
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2024-11-07 11:58:28 +0100

    evento x,y pos

--- Arbol Dom/Eventos del raton/code.js
+++ Arbol Dom/Eventos del raton/code.js
@@ -1,6 +1,10 @@
 window.onload = () =>{
 
-    document.getElementsByTagName("div")[0].onmousemove = (e) =>{
-        console.log("POSX: " + e.clientY + " POSY: " + e.clientX);
+    let circulo = document.querySelector("#circulo");
+    circulo.onmousemove = (e) =>{
+        const mouseX = e.clientX - 30;
+        const mouseY = e.clientY - 30;
+        
+        circulo.style = `transform: translate3d(${mouseX}px, ${mouseY}px, 0)`;
     }
 }
