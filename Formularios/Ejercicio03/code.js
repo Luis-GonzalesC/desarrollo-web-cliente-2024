f472e944d9760b15d18aae5a644ea4069f20afc2
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2024-12-14 14:15:01 +0100

    Ejercicio 3 finalizado

--- Formularios/Ejercicio03/code.js
+++ Formularios/Ejercicio03/code.js
@@ -3,6 +3,7 @@ window.onload = () =>{
     let span = document.getElementsByTagName("span"); ///Cogiendo el texto de cada etiqueta
     let inputs = document.querySelectorAll("input[type='checkbox']"); //Tomando todos los checkbox
     let texa= document.forms[0].elements["content"];
+
     //APARTADO A
     /*boton.addEventListener("click",
         function(ev){
@@ -15,19 +16,15 @@ window.onload = () =>{
         }, false);*/
 
     //APARTADO B
-    
+    //nextElementSibling => Devuelve el elemento hermano siguiente por el que estoy
     let i = 0;
     while (i < inputs.length){
         let checkbox = inputs[i];
         checkbox.addEventListener("click", 
             () => {
-                console.log(span);
-                console.log(span[i].textContent);
-                
-                /*
-                if(checkbox.checked) texa.value += span[i].textContent + "\n";
-                else texa.value = texa.value.replace(span[i].textContent + "\n", "");
-                //i = inputs.length;*/
+                if(checkbox.checked) texa.value += checkbox.nextElementSibling.textContent + "\n";//Saco el texto del span
+                else texa.value = texa.value.replace(checkbox.nextElementSibling.textContent + "\n", "");
+                i = inputs.length;
         }, false);
         i++;
     }
