a9575e67887a4c55422dadaca1974a7c353d0a72
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2024-12-16 21:34:46 +0100

    trabajo final avanzado

--- Trabajo_Final/JS/code.js
+++ Trabajo_Final/JS/code.js
@@ -1,8 +1,12 @@
 window.onload = () =>{
     let boton_iniciado = document.getElementsByTagName("input")[0]; //Primer boton en pantalla
     let contadorMonedas = document.getElementById("contadorMonedas");//Monedas en el HTML
-    let edificio = document.getElementsByClassName("container_edificio")[0]; //Div de los edificios
-    
+    let edificio =  document.getElementsByTagName("input")[1];; //Boton de los edificios
+
+    //Referencias al panel de los eficios
+    let panel = document.getElementsByClassName("panel_edificio")[0];
+    let cerrar_panel = document.querySelector("#cerrar_panel");
+
     let tiempo_Modena = 0.25;
     let jugador = {
         moneda: 0,
@@ -18,6 +22,19 @@ window.onload = () =>{
             }, (tiempo_Modena * jugador.moneda) * 1000); //convierto el tiempo a milisegundos porque setTimeout requiere milisegundos no segundos
         }, false)
 
+    edificio.addEventListener("click", 
+        ()=>{
+            //Cuando se haga click que se habilite una lista con los edificios
+            let panel = document.getElementsByClassName("panel_edificio")[0];
+            panel.setAttribute("style", "display: block");
+        }, false)
+    
+    cerrar_panel.addEventListener("click", 
+        ()=>{
+            //Cuando se haga click que se habilite una lista con los edificios
+            let panel = document.getElementsByClassName("panel_edificio")[0];
+            panel.setAttribute("style", "display: none");
+        }, false)
     
 }
 
