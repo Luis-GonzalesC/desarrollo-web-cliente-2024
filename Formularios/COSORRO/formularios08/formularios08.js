37317825e2e5ad23434b12e3feca387891ec7964
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2025-01-08 23:49:53 +0100

    ayuda

---
+++ Formularios/COSORRO/formularios08/formularios08.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    let formulario = document.forms[0];
+    let longitud = document.querySelectorAll("input[type='radio']").length;
+    let i = 0;
+
+    while (i < longitud) {
+        let radio = document.querySelectorAll("input[type='radio']")[i];
+        radio.addEventListener("click", () => {
+            let div = document.getElementById("alo");            
+            
+            if (radio.checked) {
+                let nuevoColor = radio.value;
+                
+                div.setAttribute("style", 
+                    `
+                    width: 100%; 
+                    height: 200px;
+                    background-color:  ${nuevoColor}`);
+            }
+        }, false);        
+        i++;
+    }
+}
