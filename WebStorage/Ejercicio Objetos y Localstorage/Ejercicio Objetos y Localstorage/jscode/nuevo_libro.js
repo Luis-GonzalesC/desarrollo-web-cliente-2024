07db64f1c16b101265d0302a49da39731e14c958
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2025-02-04 01:39:01 +0100

    Ejercicio actualizado

--- WebStorage/Ejercicio Objetos y Localstorage/Ejercicio Objetos y Localstorage/jscode/nuevo_libro.js
+++ WebStorage/Ejercicio Objetos y Localstorage/Ejercicio Objetos y Localstorage/jscode/nuevo_libro.js
@@ -1,12 +1,13 @@
 window.onload = () =>{
     let formulario = document.forms[0];
-    let libros = JSON.parse(localStorage.getItem("libros"));
     
+    let libros = JSON.parse(localStorage.getItem("libros"));
     
     formulario[8].addEventListener("click", ()=>{
         let tmp_titulo = formulario[0];
         let tmp_isbn = formulario[1];
         let tmp_paginas = formulario[2];
+        let genero = formulario[3];
         let tmp_anio = formulario[4];
         let tmp_portada = formulario[5];
         let tmp_autor = formulario[6];
@@ -15,6 +16,7 @@ window.onload = () =>{
         let nuevoLibro = {
             title: tmp_titulo.value,
             pages: tmp_paginas.value,
+            genre: genero.value,
             cover: tmp_portada.value,
             synopsis: tmp_sinopsis.value,
             year: tmp_anio.value,
@@ -25,7 +27,13 @@ window.onload = () =>{
         };
 
         libros.push(nuevoLibro);
+        console.log(libros);
+        
         localStorage.setItem("libros", JSON.stringify(libros));
     }, false);
+
+    formulario[10].addEventListener("click",function(){
+        history.back();//FUNCIÓN QUE REGRESA A LA PÁGINA ANTERIOR EN EL HISTORIAL
+    }, false)
     
 }
